99e65acb5027446d7821a41ef69233d3135be13d
shubhadip <shubhadip.maity@gmail.com>
2022-12-29 21:38:44 +0530

    refactor: cors added

--- deps.ts
+++ deps.ts
@@ -1,8 +1,10 @@
 import { Application, Router, Status } from "https://deno.land/x/oak/mod.ts";
 import { connect } from 'https://deno.land/x/redis/mod.ts';
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 export {
     Application,
     Router,
     Status,
+    oakCors,
     connect as redisConnect
 }
--- server.ts
+++ server.ts
@@ -1,11 +1,11 @@
-import { Application, Router } from "./deps.ts";
+import { Application, Router, oakCors } from "./deps.ts";
 import commonRoutes from "./src/routes/common-routes.ts";
 import indianOilRoutes from "./src/routes/indian-oil-routes.ts";
 import { PORT } from "./src/shared/constants.ts";
 
 const app = new Application();
 const router = new Router();
-
+app.use(oakCors())
 app.use(indianOilRoutes.prefix("/api/indianoilroutes").routes());
 app.use(commonRoutes.prefix("/api/common").routes());
 app.use(router.allowedMethods())
