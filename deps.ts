f2fa9fa2be657071c2bd232954ee97cf28e3094c
shubhadip <shubhadip.maity@gmail.com>
2022-12-29 17:52:45 +0530

    refactor: state wise petrol pump list for indian ooil

--- deps.ts
+++ deps.ts
@@ -1,7 +1,8 @@
 import { Application, Router, Status } from "https://deno.land/x/oak/mod.ts";
-
+import { connect } from 'https://deno.land/x/redis/mod.ts';
 export {
     Application,
     Router,
-    Status
+    Status,
+    connect as redisConnect
 }
--- server.ts
+++ server.ts
@@ -1,12 +1,13 @@
 import { Application, Router } from "./deps.ts";
 import commonRoutes from "./src/routes/common-routes.ts";
+import indianOilRoutes from "./src/routes/indian-oil-routes.ts";
 import { PORT } from "./src/shared/constants.ts";
 
 const app = new Application();
 const router = new Router();
 
+app.use(indianOilRoutes.prefix("/api/indianoilroutes").routes());
 app.use(commonRoutes.prefix("/api/common").routes());
-app.use(router.prefix("/api").routes())
 app.use(router.allowedMethods())
 
 app.addEventListener("listen" ,() => {console.log(`server started at localhost:${PORT}`)});
